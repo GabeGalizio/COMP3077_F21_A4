81b516e2c202a2cd693e6c2086bc76dc9c473bbb
daddymegaming <90657557+daddymegaming@users.noreply.github.com>
2021-11-11 22:50:07 -0500

    A little more
    Cooking Mama Lawsuit Time

--- A4 - Group 16 (Gianna, Josh, Gabe, Julia)/Assets/Scripts/SplashScreenScript.cs
+++ A4 - Group 16 (Gianna, Josh, Gabe, Julia)/Assets/Scripts/SplashScreenScript.cs
@@ -5,11 +5,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SplashScreenScript : MonoBehaviour
-{
+public class SplashScreenScript : MonoBehaviour {
+
+    [SerializeField] private float delay;
+    public AudioClip pressSFX;
+    public AudioSource audio;
+
     void Update() {
         if(Input.anyKey) {
-            SceneManager.LoadScene("Main Menu");
+            audio.PlayOneShot(pressSFX, 0.7f);
+            StartCoroutine(SceneTransition(delay));
         }
+    }
+
+    //Coroutine to delay transition to main menu (so sound effect has time to play.)
+    public IEnumerator SceneTransition(float delayTime) {
+        yield return new WaitForSeconds(delayTime);
+        SceneManager.LoadScene("Main Menu");
     }
 }
